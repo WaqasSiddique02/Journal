f55926ad2ef1280cdb29ce67302adb5341c9279c
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-28 19:19:50 +0500

    Complete Front End

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var ejs=require('ejs');
+var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var signupRouter= require('./routes/register');
+var composeRouter=require('./routes/compose');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login',loginRouter);
 app.use('/register',signupRouter);
+app.use('/compose',composeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
