77258e7b482a75fb01470d0e2e8a317555836162
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-29 12:06:39 +0500

    Added data insertion functionality

--- routes/compose.js
+++ routes/compose.js
@@ -11,21 +11,17 @@ router.post('/', async function(req,res){
   const title=req.body.title;
   const content=req.body.content;
   const date = new Date().toISOString();
-  if(!title||!content){
-    return res.redirect('/compose?error=Please%20fill%20in%20all%20the%20fields');
-  }
-  else{
+
     try{
-      const query=`INSERT INTO userData (title, content ,date) VALUES (${title},${content},${date})`;
+      const query = `INSERT INTO userData (title, content, date) VALUES ('${title}', '${content}', '${date}')`;
       await sql.query(query);
       console.log("Data inserted Succesfully");
+      res.redirect('/');
     }
     catch(err){
       console.log(err);
       res.send("Error,Submiting try again");
     }
-  }
-  res.redirect('/');
 });
 
 module.exports = router;
